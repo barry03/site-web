1cb53fa67bacc544574831d591e9e7c85f5965b8
Barry Mamadou Djoulde <barry_m@iuct-oncopole.fr>
2025-04-15 11:27:30 +0200

    mise à de la section compétence et stack

--- citation.js
+++ citation.js
@@ -1,12 +1,12 @@
 
 const quotes = {
     en: [
-        `"In God we trust. All others must bring data." – W. Edwards Deming`,
+        //`"In God we trust. All others must bring data." – W. Edwards Deming`,
         `"Code is like humor. When you have to explain it, it’s bad."`,
         `"Premature optimization is the root of all evil." – Donald Knuth`
     ],
     fr: [
-        `"En Dieu nous croyons. Tous les autres doivent apporter des données." – W. Edwards Deming`,
+        //`"En Dieu nous croyons. Tous les autres doivent apporter des données." – W. Edwards Deming`,
         `"Le code est comme l'humour. Si vous devez l'expliquer, c’est qu’il est mauvais."`,
         `"L’optimisation prématurée est la racine de tout mal." – Donald Knuth`
     ]
